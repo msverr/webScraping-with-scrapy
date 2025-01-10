eeb4979cb52b4f46564ba17339fa51524b4f0471
msverr <denis.s.kobets@gmail.com>
2025-01-10 13:23:33 +0200

    parsed book_data to json

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -10,12 +10,14 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css("article.product_pod")
 
         for book in books:
-            yield {
-                "name": book.css("h3 a::text").get(),
-                "price": book.css("p.price_color::text").get(),
-                "url": book.css("h3 a").attrib["href"],
-            }
+            relative_url = book.css("h3 a").attrib["href"]
 
+            if "catalogue/" in relative_url:
+                book_url = f"https://books.toscrape.com/{relative_url}"
+            else:
+                book_url = f"https://books.toscrape.com/catalogue/{relative_url}"
+            yield response.follow(book_url, callback=self.parse_book_page)
+            
         next_page = response.css("li.next a").attrib["href"]
 
         if next_page:
@@ -24,3 +26,26 @@ class BookspiderSpider(scrapy.Spider):
             else:
                 next_page_url = f"https://books.toscrape.com/catalogue/{next_page}"
             yield response.follow(next_page_url, callback=self.parse)
+        
+
+    def parse_book_page(self, response):
+        table_rows = response.css("table tr")
+
+        yield {
+            "url": response.url,
+            "title": response.css(".product_main h1::text").get(),
+            "product_type": table_rows[1].css("td ::text").get(),
+            "price_excl_tax": table_rows[2].css("td ::text").get(),
+            "price_incl_tax": table_rows[3].css("td ::text").get(),
+            "tax": table_rows[4].css("td ::text").get(),
+            "availability": table_rows[5].css("td ::text").get(),
+            "num_reviews": table_rows[6].css("td ::text").get(),
+            "stars": response.css("p.star-rating").attrib["class"].strip(" ")[1],
+            "category": response.xpath(
+                "//ul[@class='breadcrumb']/li[3]/a/text()"
+            ).get(),
+            "description": response.xpath(
+                "//div[@id='product_description']/following-sibling::p/text()"
+            ).get(),
+            'price': response.css("p.price_color::text").get(),
+        }
